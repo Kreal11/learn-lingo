b1320c9aa4b331cdf1ecdc772444599d61a6e9f2
Kreal11 <kirillshyrokov2@gmail.com>
2024-04-13 15:29:28 +0300

    added reading data from DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/home/Home";
+import Teachers from "./pages/teachers/Teachers";
 import "./styles/App.css";
 
 function App() {
   return (
     <>
       <Home />
+      <Teachers />
     </>
   );
 }
