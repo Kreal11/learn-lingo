6f2bc97bb8cb6d2d30df46e9ca8a5d77f5457386
Kreal11 <kirillshyrokov2@gmail.com>
2024-04-18 15:01:03 +0300

    added global styles, fonts and modern-normalize

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./styles/index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme.js";
+import { GlobalStyle } from "./globalStyles.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter basename="/">
     <ThemeProvider theme={theme}>
       <App />
+      <GlobalStyle />
     </ThemeProvider>
   </BrowserRouter>
   // </React.StrictMode>,
